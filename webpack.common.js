10f294428713e96ea4daf9a876a3ec64c52e98ba
mds <17163988+mds1@users.noreply.github.com>
2017-11-21 12:34:43 -0500

    Added index.html template, removed clean webpack plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,15 @@
 const path = require('path');
-// const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: ['babel-polyfill', './src/main.js'],
-    // plugins: [
-    //     new CleanWebpackPlugin(['dist']),
-    // ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            template: 'index.html'
+        })
+    ],
     module: {
-        // loaders: [
-        //     { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-        // ],
         rules: [
             {
                 test: /\.css$/,
